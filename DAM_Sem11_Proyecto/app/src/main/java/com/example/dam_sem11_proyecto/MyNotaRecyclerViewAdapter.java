35e17d04a853873e512d1bb0d253bc44d522393a
Thalia Mabel <20A3050036@untels.edu.pe>
2023-06-26 17:30:46 -0500

    DAM_S13

---
+++ DAM_Sem11_Proyecto/app/src/main/java/com/example/dam_sem11_proyecto/MyNotaRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.dam_sem11_proyecto;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.dam_sem11_proyecto.databinding.FragmentNotaBinding;
+
+import java.util.List;
+
+public class MyNotaRecyclerViewAdapter extends RecyclerView.Adapter<MyNotaRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Nota> mValues;
+    private final NotasInteractionListener mListener;
+
+    public MyNotaRecyclerViewAdapter(List<Nota> items, NotasInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentNotaBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Nota mItem;
+
+        public ViewHolder(FragmentNotaBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
